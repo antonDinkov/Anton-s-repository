3f7977304ceb4fddc11498bc53914163a0f44f59
antonDinkov <166150125+antonDinkov@users.noreply.github.com>
2024-08-21 22:24:04 +0200

    Add files via upload

---
+++ 01.personInfo.js
@@ -0,0 +1,13 @@
+function personInfo(firstName, lastName, age){
+
+const person = {
+    firstName,
+    lastName,
+    age
+}
+return person
+}
+personInfo("Peter","Pan","20");
+//personInfo("George","Smith","18");
+let output =personInfo("Peter","Pan","20");
+console.log(output);
