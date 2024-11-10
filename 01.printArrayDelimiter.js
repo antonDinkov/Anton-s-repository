d0b269bf67045b54c15e79fcf61eca51fb5eea6f
antonDinkov <166150125+antonDinkov@users.noreply.github.com>
2024-11-10 09:39:48 +0100

    Add files via upload

---
+++ 01.printArrayDelimiter.js
@@ -0,0 +1,29 @@
+function printArrayDelimiter(arr, delimeter){
+
+console.log(arr.join(delimeter));
+
+}
+printArrayDelimiter(['One',  
+
+    'Two',  
+    
+    'Three',  
+    
+    'Four',  
+    
+    'Five'],  
+    
+    '-');
+printArrayDelimiter(['How about no?',  
+
+    'I', 
+    
+    'will',  
+    
+    'not',  
+    
+    'do',  
+    
+    'it!'],  
+    
+    '_')
