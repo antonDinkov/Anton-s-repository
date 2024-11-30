3d59f45146b98c2a23b1cac4dc4a90b0983b5af1
antonDinkov <166150125+antonDinkov@users.noreply.github.com>
2024-11-30 17:22:17 +0100

    Add files via upload

---
+++ 01.cityRecord.js
@@ -0,0 +1,20 @@
+function cityRecord(name, population, treasury){
+
+let obj = {
+    name,
+    population,
+    treasury
+}
+return obj;
+
+}
+cityRecord('Tortuga', 
+
+7000, 
+
+15000)
+cityRecord('Santo Domingo', 
+
+12000, 
+
+23500)
