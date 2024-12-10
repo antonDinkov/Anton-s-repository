c2c68c1b580f611631c0aa3dbbd575ba13a392d0
antonDinkov <166150125+antonDinkov@users.noreply.github.com>
2024-12-10 22:24:12 +0100

    Add files via upload

---
+++ 03.currencyFormat.js
@@ -0,0 +1,19 @@
+function currencyFormat(separator, symbol, symbolFirst, formatter){
+
+return (value) => formatter(separator, symbol, symbolFirst, value);
+
+}
+
+function currencyFormatter(separator, symbol, symbolFirst, value) { 
+
+    let result = Math.trunc(value) + separator; 
+
+    result += value.toFixed(2).substr(-2,2); 
+
+    if (symbolFirst) return symbol + ' ' + result; 
+
+    else return result + ' ' + symbol; 
+
+} 
+
+console.log(currencyFormatter(5345));
