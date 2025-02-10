bfa5e1560d6e4d7d07964b89eea08ffb5c0186a1
antonDinkov <166150125+antonDinkov@users.noreply.github.com>
2025-02-10 19:30:50 +0100

    Add files via upload

---
+++ 06.melrahShake.js
@@ -0,0 +1,13 @@
+function melrahShake(arr){
+
+let str = arr.shift();
+let pattern = arr.shift();
+
+
+}
+melrahShake(['astalavista baby', 
+
+    'sta'])
+/* melrahShake(['##mtm!!mm.mm*mtm.#', 
+
+    'mtm']) */
